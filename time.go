0ca290f35f3ffb56bc202570eb9652aca3fcd6f7
tocurd <tocurd@qq.com>
2021-06-04 23:00:04 +0800

    add time util

--- time.go
+++ time.go
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-06-04 22:28:48
- * @LastEditTime: 2021-06-04 22:43:52
+ * @LastEditTime: 2021-06-04 22:59:50
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \webServeri:\project\goUtils\time.go
  */
-package time
+package main
 
 import "time"
 
@@ -16,37 +16,37 @@ type TimeUtil struct {
 }
 
 // 当前时间
-func (utilTime *Time) GetTime() time.Time {
+func (utilTime *TimeUtil) GetTime() time.Time {
 	return time.Now()
 }
 
 // 格式化为:20060102150405
-func (utilTime *Time) GetTimeString(t time.Time) string {
+func (utilTime *TimeUtil) GetTimeString(t time.Time) string {
 	return t.Format(utilTime.TimeFormat)
 }
 
 // 格式化为:2006-01-02 15:04:05
-func (utilTime *Time) GetNormalTimeString(t time.Time) string {
+func (utilTime *TimeUtil) GetNormalTimeString(t time.Time) string {
 	return t.Format(utilTime.NormalTimeFormat)
 }
 
 // 转为时间戳->秒数
-func (utilTime *Time) GetTimeUnix(t time.Time) int64 {
+func (utilTime *TimeUtil) GetTimeUnix(t time.Time) int64 {
 	return t.Unix()
 }
 
 // 转为时间戳->毫秒数
-func (utilTime *Time) GetTimeMills(t time.Time) int64 {
+func (utilTime *TimeUtil) GetTimeMills(t time.Time) int64 {
 	return t.UnixNano() / 1e6
 }
 
 // 时间戳转时间
-func (utilTime *Time) GetTimeByInt(t1 int64) time.Time {
+func (utilTime *TimeUtil) GetTimeByInt(t1 int64) time.Time {
 	return time.Unix(t1, 0)
 }
 
 // 字符串转时间
-func (utilTime *Time) GetTimeByString(timestring string) (time.Time, error) {
+func (utilTime *TimeUtil) GetTimeByString(timestring string) (time.Time, error) {
 	if timestring == "" {
 		return time.Time{}, nil
 	}
@@ -54,7 +54,7 @@ func (utilTime *Time) GetTimeByString(timestring string) (time.Time, error) {
 }
 
 // 标准字符串转时间
-func (utilTime *Time) GetTimeByNormalString(timestring string) (time.Time, error) {
+func (utilTime *TimeUtil) GetTimeByNormalString(timestring string) (time.Time, error) {
 	if timestring == "" {
 		return time.Time{}, nil
 	}
@@ -62,19 +62,19 @@ func (utilTime *Time) GetTimeByNormalString(timestring string) (time.Time, error
 }
 
 // 比较两个时间大小
-func (utilTime *Time) CompareTime(t1, t2 time.Time) bool {
+func (utilTime *TimeUtil) CompareTime(t1, t2 time.Time) bool {
 	return t1.Before(t2)
 }
 
 // n小时后的时间字符串
-func (utilTime *Time) GetNextHourTime(s string, n int64) string {
+func (utilTime *TimeUtil) GetNextHourTime(s string, n int64) string {
 	t2, _ := time.ParseInLocation(utilTime.TimeFormat, s, time.Local)
 	t1 := t2.Add(time.Hour * time.Duration(n))
 	return utilTime.GetTimeString(t1)
 }
 
 // 计算俩个时间差多少小时
-func (utilTime *Time) GetHourDiffer(start_time, end_time string) float32 {
+func (utilTime *TimeUtil) GetHourDiffer(start_time, end_time string) float32 {
 	var hour float32
 	t1, _ := time.ParseInLocation(utilTime.TimeFormat, start_time, time.Local)
 	t2, err := time.ParseInLocation(utilTime.TimeFormat, end_time, time.Local)
@@ -87,7 +87,7 @@ func (utilTime *Time) GetHourDiffer(start_time, end_time string) float32 {
 }
 
 // 判断当前时间是否是整点
-func (utilTime *Time) Checkhours() bool {
+func (utilTime *TimeUtil) Checkhours() bool {
 	_, m, s := utilTime.GetTime().Clock()
 	if m == s && m == 0 && s == 0 {
 		return true
