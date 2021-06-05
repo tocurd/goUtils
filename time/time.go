f6158b10feccbdb954e0c1f6bc72feab4cbd2dd7
tocurd <tocurd@qq.com>
2021-06-05 10:46:21 +0800

    update

--- time/time.go
+++ time/time.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-04 22:28:48
- * @LastEditTime: 2021-06-05 10:40:50
+ * @LastEditTime: 2021-06-05 10:45:24
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edits
  * @FilePath: \webServeri:\project\goUtils\time.go
@@ -10,37 +10,65 @@ package goUtils
 
 import "time"
 
-// 当前时间
+/**
+ * @description: 获取今日0点的时间戳
+ * @param {*}
+ * @return {*}
+ */
+// func Get
+
+/**
+ * @description: 当前时间
+ * @param {*}
+ * @return {*}
+ */
 func GetTime() time.Time {
 	return time.Now()
 }
 
-// 格式化为:20060102150405
+/**
+ * @description: 将时间转换成指定字符串
+ * @param {time.Time} t 时间
+ * @param {string} timeFormat 时间格式
+ * @return {*}
+ */
 func GetTimeString(t time.Time, timeFormat string) string {
 	return t.Format(timeFormat)
 }
 
-// 格式化为:2006-01-02 15:04:05
-func GetNormalTimeString(t time.Time, normalTimeFormat string) string {
-	return t.Format(normalTimeFormat)
-}
-
-// 转为时间戳->秒数
+/**
+ * @description: 将时间转换成时间戳
+ * @param {time.Time} t
+ * @return {*}
+ */
 func GetTimeUnix(t time.Time) int64 {
 	return t.Unix()
 }
 
-// 转为时间戳->毫秒数
+/**
+ * @description: 将秒级时间戳转换成毫秒
+ * @param {time.Time} t
+ * @return {*}
+ */
 func GetTimeMills(t time.Time) int64 {
 	return t.UnixNano() / 1e6
 }
 
-// 时间戳转时间
+/**
+ * @description: 将时间戳转换成时间
+ * @param {int64} t1
+ * @return {*}
+ */
 func GetTimeByInt(t1 int64) time.Time {
 	return time.Unix(t1, 0)
 }
 
-// 字符串转时间
+/**
+ * @description: 将字符串转换成时间格式
+ * @param {string} timestring
+ * @param {string} timeFormat 字符串的格式
+ * @return {*}
+ */
 func GetTimeByString(timestring string, timeFormat string) (time.Time, error) {
 	if timestring == "" {
 		return time.Time{}, nil
@@ -56,19 +84,36 @@ func GetTimeByNormalString(timestring string, normalTimeFormat string) (time.Tim
 	return time.ParseInLocation(normalTimeFormat, timestring, time.Local)
 }
 
-// 比较两个时间大小
+/**
+ * @description: 比较两个时间大小
+ * @param {*} t1
+ * @param {time.Time} t2
+ * @return {*}
+ */
 func CompareTime(t1, t2 time.Time) bool {
 	return t1.Before(t2)
 }
 
-// n小时后的时间字符串
+/**
+ * @description: 获取基于当前时间N小时候的时间字符串
+ * @param {string} s
+ * @param {int64} n
+ * @param {string} timeFormat
+ * @return {*}
+ */
 func GetNextHourTime(s string, n int64, timeFormat string) string {
 	t2, _ := time.ParseInLocation(timeFormat, s, time.Local)
 	t1 := t2.Add(time.Hour * time.Duration(n))
 	return GetTimeString(t1, timeFormat)
 }
 
-// 计算俩个时间差多少小时
+/**
+ * @description: 计算俩个时间差多少小时
+ * @param {*} start_time
+ * @param {string} end_time
+ * @param {string} timeFormat
+ * @return {*}
+ */
 func GetHourDiffer(start_time, end_time string, timeFormat string) float32 {
 	var hour float32
 	t1, _ := time.ParseInLocation(timeFormat, start_time, time.Local)
@@ -81,7 +126,11 @@ func GetHourDiffer(start_time, end_time string, timeFormat string) float32 {
 	return hour
 }
 
-// 判断当前时间是否是整点
+/**
+ * @description: 判断当前时间是否是整点
+ * @param {*}
+ * @return {*}
+ */
 func Checkhours() bool {
 	_, m, s := GetTime().Clock()
 	if m == s && m == 0 && s == 0 {
