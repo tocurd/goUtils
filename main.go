cec05d8831d9662a340f5b691764b0b4b159075c
tocurd <tocurd@qq.com>
2021-06-05 09:06:25 +0800

    update

---
+++ main.go
@@ -0,0 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2021-06-05 09:05:03
+ * @LastEditTime: 2021-06-05 09:06:02
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \webServeri:\project\goUtils\main.go
+ */
+
+func main() {
+
+}
