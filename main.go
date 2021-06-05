90db1a80a81329ed9e9e9422b324e60e28054694
tocurd <tocurd@qq.com>
2021-06-05 10:18:07 +0800

    update

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2021-06-05 09:05:03
- * @LastEditTime: 2021-06-05 09:06:02
+ * @LastEditTime: 2021-06-05 10:17:52
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \webServeri:\project\goUtils\main.go
  */
+package main
 
 func main() {
 
